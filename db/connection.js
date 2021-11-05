20eaaf5ef81259990cdcea6885a5f5b969b5507a
Brynne Eastman <brynne.eastman@gmail.com>
2021-11-05 07:12:23 -0400

    modularize the application

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+//Connect to the database
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'Mickeymouse111!',
+  database: 'election'
+});
+
+module.exports = db;
